f2a54b98f5542861623d81aa286d97ec0ebd595c
Michael Giambalvo <heathkit@google.com>
2016-05-10 10:55:06 -0700

    chore(lib): Convert to TypeScript.

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,7 @@ var runSpawn = function(done, task, opt_arg) {
 
 gulp.task('built:copy', function() {
   return gulp.src(['lib/**/*','!lib/**/*.ts'])
-      .pipe(gulp.dest('built/'));
+      .pipe(gulp.dest('built/lib/'));
 });
 
 gulp.task('webdriver:update', function(done) {
@@ -26,9 +26,17 @@ gulp.task('jslint', function(done) {
   runSpawn(done, './node_modules/.bin/jshint', ['lib','spec', 'scripts']);
 });
 
-gulp.task('clang', function() {
-  return gulp.src(['lib/**/*.ts', 'lib/**/*.js', 'spec/**/*.js'])
+gulp.task('clang-check', function() {
+  return gulp.src(['src/**/*.ts'])
       .pipe(gulpFormat.checkFormat('file', clangFormat))
+      .on('warning', function(e) {
+    console.log(e);
+  });
+});
+
+gulp.task('clang', function() {
+  return gulp.src(['src/**/*.ts'])
+      .pipe(gulpFormat.format('file', clangFormat))
       .on('warning', function(e) {
     console.log(e);
   });
@@ -39,7 +47,7 @@ gulp.task('typings', function(done) {
 });
 
 gulp.task('tsc', function(done) {
-  runSpawn(done, 'node_modules/typescript/bin/tsc');
+  runSpawn(done, 'node', ['node_modules/typescript/bin/tsc']);
 });
 
 gulp.task('prepublish', function(done) {
@@ -49,4 +57,26 @@ gulp.task('prepublish', function(done) {
 gulp.task('pretest', function(done) {
   runSequence(
     ['webdriver:update', 'typings', 'jslint', 'clang'], 'tsc', 'built:copy', done);
+});
+
+gulp.task('default', ['prepublish']);
+gulp.task('build', ['prepublish']);
+
+gulp.task('test:copy', function(done) {
+  return gulp.src(['spec/**/*','!spec/**/*.ts'])
+      .pipe(gulp.dest('built/spec/'));
+});
+
+gulp.task('test', ['build', 'test:copy'], function(done) {
+  var opt_arg = [];
+  opt_arg.push('node_modules/jasmine/bin/jasmine.js');
+  opt_arg.push('JASMINE_CONFIG_PATH=spec/unit_config.json');
+  runSpawn(done, 'node', opt_arg);
+});
+
+gulp.task('test:e2e', ['build', 'test:copy'], function(done) {
+  var opt_arg = [];
+  opt_arg.push('node_modules/jasmine/bin/jasmine.js');
+  opt_arg.push('JASMINE_CONFIG_PATH=spec/e2e_config.json');
+  runSpawn(done, 'node', opt_arg);
 });
