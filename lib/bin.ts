927d467295465ea29ed65595c27e3bb8fce661d0
Michael Giambalvo <heathkit@google.com>
2016-08-08 14:35:42 -0700

    chore(launcher): Rename launch script and to package.json

--- lib/bin.ts
+++ lib/bin.ts
@@ -1,9 +1,13 @@
+#!/usr/bin/env node
+
+var path = require('path');
+
 /**
  * Starts up a proxy server which modifies calls between the test process
  * and the selenium server.
  */
 
-var BlockingProxy = require('./built/lib/blockingproxy').BlockingProxy;
+var BlockingProxy = require('./blockingproxy').BlockingProxy;
 
 var proxy = new BlockingProxy();
 proxy.listen(8111);
